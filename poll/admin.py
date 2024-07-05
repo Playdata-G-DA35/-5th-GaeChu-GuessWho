12175e0e9987a1d15802c473e860212c5e5a79b6
eumwoo <coop1964@naver.com>
2024-07-05 15:24:26 +0900

    del all

--- poll/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Image_path)
-admin.site.register(Ans_vote)
