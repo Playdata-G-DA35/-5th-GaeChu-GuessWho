2cfe6bb2ac6c34a812853ae256ae051a83515fc4
unknown <ihman0513@naver.com>
2024-07-07 23:47:34 +0900

    .

--- poll/urls.py
+++ poll/urls.py
@@ -6,5 +6,6 @@ app_name = 'poll'
 urlpatterns = [
     path("cat_list", views.cat_list, name="cat_list"),
     path("img_list/cat<int:category_id>", views.img_list, name="img_list"),
-    path("ans_vote/cat<int:category_id>/<str:img_name>", views.ans_vote, name="ans_vote")
+    path("ans_vote/cat<int:category_id>/<str:img_name>", views.ans_vote, name="ans_vote"),
+    path("ans_vote/cat<int:category_id>/<str:img_name>", views.ans_create, name="ans_create"),
 ]
--- poll/views.py
+++ poll/views.py
@@ -22,9 +22,20 @@ def img_list(request, category_id):
 
 def ans_vote(request, category_id, img_name):
     i = Image_path.objects.get(img_name=img_name, category=category_id)
+    ans_list = Ans_vote.objects.filter(img_path = i)
     #c = Category(c = i.category)
 
-    return render(request, "poll/ans_vote.html", {'cid_of_views': category_id, 'img_path': i.img_path})
+    return render(request, "poll/ans_vote.html", {'category_id': category_id, 'img_name': i.img_name, 'ans_list': ans_list})
 
-#def create_ans_vote(request):
-#    ans_vote = Ans_vote()
+def ans_create(request, category_id, img_name):
+    i = Image_path.objects.get(img_name=img_name, category=category_id)
+
+    if request.method == 'POST':
+        user = request.user
+        comment = request.POST.get('content')
+        if comment:  # comment가 비어 있지 않은지 확인
+            #Ans_vote.objects.create(img_path=i, ans_text=comment, votes=0)
+            ans = Ans_vote(img_path= i, ans_text= comment, votes= 0)
+            ans.save()
+
+    return redirect('poll:ans_vote', category_id=category_id, img_name=img_name)
